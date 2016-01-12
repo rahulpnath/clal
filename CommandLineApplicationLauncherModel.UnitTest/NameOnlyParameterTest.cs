1b571de9de364bf97ff6818d7fe63a23c8b05715
Rahul P Nath <rahulpnath@gmail.com>
2016-01-13 01:27:37 +1100

    Added NameOnlyParameter class

---
+++ CommandLineApplicationLauncherModel.UnitTest/NameOnlyParameterTest.cs
@@ -0,0 +1,38 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherModel.UnitTest
+{
+    public class NameOnlyParameterTest
+    {
+        [Theory, AutoData]
+        public void CtorWithNullArgumentsThrowsException(IFixture fixture)
+        {
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(NameOnlyParameter));
+        }
+
+        [Theory, AutoData]
+        public void CtorParametersAreInitialized(IFixture fixture)
+        {
+            var assertion = new ConstructorInitializedMemberAssertion(fixture);
+            assertion.Verify(typeof(NameOnlyParameter));
+        }
+
+        [Theory, AutoData]
+        public void SutWithSameValuesAreEqual(Name name)
+        {
+            var aParameter = new NameOnlyParameter(name);
+            var anotherParameter = new NameOnlyParameter(name);
+            Assert.Equal(aParameter, anotherParameter);
+            Assert.Equal(aParameter.GetHashCode(), anotherParameter.GetHashCode());
+        }
+    }
+}
---
+++ CommandLineApplicationLauncherModel/NameOnlyParameter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApplicationLauncherModel
+{
+    public class NameOnlyParameter : IParameter
+    {
+        public Name Name { get; private set; }
+
+        public NameOnlyParameter(Name name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var objAsParameter = obj as NameOnlyParameter;
+            if (objAsParameter == null)
+                return false;
+
+            return objAsParameter.Name == this.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+    }
+}
