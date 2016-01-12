0cffd4efd48c76bd35c2b031cd0549699ea1a3a5
Rahul P Nath <rahulpnath@gmail.com>
2016-01-12 23:41:18 +1100

    added parametermeta class

---
+++ CommandLineApplicationLauncherModel.UnitTest/ParameterMetaTest.cs
@@ -0,0 +1,17 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace CommandLineApplicationLauncherModel.UnitTest
+{
+    public class ParameterMetaTest
+    {
+        [Theory,AutoData]
+        public void NullForCtorArgumentsThrowsException(IFixture fixture)
+        {
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(ParameterMeta));
+        }
+    }
+}
---
+++ CommandLineApplicationLauncherModel/ParameterMeta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApplicationLauncherModel
+{
+    public class ParameterMeta
+    {
+        public Name Name { get; private set; }
+
+        public ParameterMeta(Name name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            this.Name = name;
+        }
+    }
+}
