b0288853d66bea65a6d084af99d6cf8e5ec86fd3
Rahul P Nath <rahulpnath@gmail.com>
2016-01-16 01:23:53 +1100

    Added cmdapplicaiton

---
+++ CommandLineApplicationLauncherModel.UnitTest/CmdApplicationTest.cs
@@ -0,0 +1,35 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherModel.UnitTest
+{
+    public class CmdApplicationTest
+    {
+        [Theory, AutoData]
+        public void NullForCtorArgumentsThrowsException(IFixture fixture, Name name)
+        {
+            fixture.Inject<IParameter>(new ParameterTest());
+            fixture.Inject<ParameterMeta>(ParameterMeta.Create<IParameter>(name));
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(CmdApplication).GetConstructors());
+        }
+
+        [Theory, AutoData]
+        public void CtorParametersAreInitialized(IFixture fixture, Name name)
+        {
+            fixture.Inject<IParameter>(new ParameterTest());
+            fixture.Inject<ParameterMeta>(ParameterMeta.Create<IParameter>(name));
+            var assertion = new ConstructorInitializedMemberAssertion(fixture);
+            assertion.Verify(typeof(CmdApplication));
+        }
+
+        private class ParameterTest : IParameter { }
+    }
+}
---
+++ CommandLineApplicationLauncherModel/CmdApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApplicationLauncherModel
+{
+    public class CmdApplication
+    {
+        public Name Name { get; private set; }
+        public Name ApplicationName { get; private set; }
+        public ReadOnlyCollection<IParameter> Parameters { get; private set; }
+
+        public CmdApplication(Name name, Name applicationName, ReadOnlyCollection<IParameter> parameters)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            if (applicationName == null)
+                throw new ArgumentNullException(nameof(applicationName));
+
+            if (parameters == null)
+                throw new ArgumentNullException(nameof(parameters));
+
+            this.Name = name;
+            this.ApplicationName = applicationName;
+            this.Parameters = parameters;
+        }
+    }
+}
