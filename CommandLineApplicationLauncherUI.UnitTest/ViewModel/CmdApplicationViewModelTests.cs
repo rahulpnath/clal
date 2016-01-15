7935d3fc976c8387e3e849888dac3ff5fb9953a2
Rahul P Nath <rahulpnath@gmail.com>
2016-01-16 01:31:51 +1100

    Added first view model and related packages from mvvmlight

---
+++ CommandLineApplicationLauncherUI.UnitTest/ViewModel/CmdApplicationViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using CommandLineApplicationLauncherUI.ViewModel;
+using GalaSoft.MvvmLight;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherUI.UnitTest.ViewModel
+{
+    public class CmdApplicationViewModelTests
+    {
+
+        [Theory, AutoData]
+        public void SutIsViewModelBase(CmdApplicationViewModel sut)
+        {
+            Assert.IsAssignableFrom<ViewModelBase>(sut);
+        }
+    }
+}
