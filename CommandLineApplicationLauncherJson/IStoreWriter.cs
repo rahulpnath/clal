b9ccd7af82635b7fe53be54feea9897f4f775cbb
Rahul P Nath <rahulpnath@gmail.com>
2016-01-24 11:59:23 +1100

    Added projet for handling json converisons

---
+++ CommandLineApplicationLauncherJson/IStoreWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommandLineApplicationLauncherModel;
+using System.IO;
+
+namespace CommandLineApplicationLauncherJson
+{
+    public interface IStoreWriter<T> where T : IMessage
+    {
+        Stream OpenStreamFor(T message);
+    }
+}
---
+++ CommandLineApplicationLauncherJson/JsonChannel.cs
@@ -0,0 +1,30 @@
+﻿using CommandLineApplicationLauncherModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApplicationLauncherJson
+{
+    public class JsonChannel<T> : IChannel<T> where T : IMessage
+    {
+        private readonly JsonSerializer serializer;
+        private readonly IStoreWriter<T> store;
+
+        public JsonChannel(IStoreWriter<T> store)
+        {
+            this.store = store;
+            this.serializer = new JsonSerializer();
+        }
+
+        public void Send(T message)
+        {
+            using (var stream = this.store.OpenStreamFor(message))
+            using (var writer = new StreamWriter(stream))
+                this.serializer.Serialize(writer, message);
+        }
+    }
+}
