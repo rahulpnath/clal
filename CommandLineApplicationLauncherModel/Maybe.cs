280b914da33f2db8b195b0fe8653bb4510b05be9
Rahul P Nath <rahulpnath@gmail.com>
2016-01-31 12:02:47 +1100

    Added maybe class

---
+++ CommandLineApplicationLauncherModel/Maybe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineApplicationLauncherModel
+{
+    public class Maybe<T> : IEnumerable<T>
+    {
+        private IEnumerable<T> values;
+
+        public Maybe()
+        {
+            this.values = new T[0];
+        }
+
+        public Maybe(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            this.values = new T[] { value };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.values.GetEnumerator();
+        }
+    }
+
+    public static class Maybe
+    {
+        public static Maybe<T> ToMaybe<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+
+        public static Maybe<T> Empty<T>()
+        {
+            return new Maybe<T>();
+        }
+    }
+}
