07bbf64434a6b2215a206e98e66d6afd9ce5a978
Rahul P Nath <rahulpnath@gmail.com>
2016-02-02 06:40:16 +1100

    Added CmdApplicationConfigurationListViewModel

---
+++ src/CommandLineApplicationLauncherUI.UnitTest/ViewModel/CmdApplicationConfigurationListViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using CommandLineApplicationLauncherUI.ViewModel;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherUI.UnitTest.ViewModel
+{
+    public class CmdApplicationConfigurationListViewModelTests
+    {
+        [Theory, AutoMoqData]
+        public void SutIsViewModelBase(CmdApplicationConfigurationListViewModel sut)
+        {
+            Assert.IsAssignableFrom<ViewModelBase>(sut);
+        }
+    }
+}
