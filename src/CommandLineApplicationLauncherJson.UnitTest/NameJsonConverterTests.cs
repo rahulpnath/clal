a57e288d80f3f66d835d8171792dc67d98edc188
Rahul P Nath <rahulpnath@gmail.com>
2016-02-02 13:18:05 +1100

    Added namejsonconvertertest and fixed few failing tests

---
+++ src/CommandLineApplicationLauncherJson.UnitTest/NameJsonConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherJson.UnitTest
+{
+    public class NameJsonConverterTests
+    {
+        [Theory, AutoMoqData]
+        public void SutIsJsonConverter(NameJsonConverter sut)
+        {
+            Assert.IsAssignableFrom<JsonConverter>(sut);
+        }
+    }
+}
