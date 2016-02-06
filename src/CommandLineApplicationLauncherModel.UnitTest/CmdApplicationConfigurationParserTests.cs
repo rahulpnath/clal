f6f0e57fef81282a58896ef1f934838e5f995a0d
Rahul P Nath <rahulpnath@gmail.com>
2016-02-06 11:40:46 +1100

    Added in parser for parsing from various sources

---
+++ src/CommandLineApplicationLauncherModel.UnitTest/CmdApplicationConfigurationParserTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandLineApplicationLauncherModel.UnitTest
+{
+    public class CmdApplicationConfigurationParserTests
+    {
+        [Fact]
+        public void SutIsAbstract()
+        {
+            Assert.True(typeof(CmdApplicationConfigurationParser<>).IsAbstract);
+        }
+
+        [Fact]
+        public void SutReturnsEmptyWhenParametersIsNullOrEmpty()
+        {
+            var expected = Maybe.Empty<CmdApplicationConfiguration>();
+            var name = (Name)"AnyName";
+            var parser = new TestParser(name, null);
+            var actual = parser.Parse(new object(), GetApplicationConfigurationMeta());
+            Assert.Equal(expected, actual);
+        }
+
+        private CmdApplicationMeta GetApplicationConfigurationMeta()
+        {
+            return new CmdApplicationMeta(
+                    (Name)"FriendlyName",
+                    (Name)"ApplicationName",
+                    new List<ParameterMeta>()
+                    {
+                        ParameterMeta.Create<NameOnlyParameter>((Name)"aParameter"),
+                        ParameterMeta.Create<NameValueParameter>((Name)"anotherParameter")
+                    });
+                    
+        }
+    }
+
+    class TestParser : CmdApplicationConfigurationParser<object>
+    {
+        public Name FriendlyName { get; private set; }
+        public List<IParameter> Parameters { get; private set; }
+
+        public TestParser(Name friendlyName, List<IParameter> parameters)
+        {
+            this.FriendlyName = friendlyName;
+            this.Parameters = parameters;
+        }
+
+        protected override Name GetFriendlyName(object data, CmdApplicationMeta applicationMeta)
+        {
+            return FriendlyName;
+        }
+
+        protected override IList<IParameter> GetParameters(object data, CmdApplicationMeta applicationMeta)
+        {
+            return Parameters;
+        }
+    }
+}
---
+++ src/CommandLineApplicationLauncherModel/CmdApplicationConfigurationParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CommandLineApplicationLauncherModel
+{
+    public abstract class CmdApplicationConfigurationParser<T>
+    {
+        public Maybe<CmdApplicationConfiguration> Parse(T data, CmdApplicationMeta applicationMeta)
+        {
+            var friendlyName = this.GetFriendlyName(data, applicationMeta);
+            var parameters = this.GetParameters(data, applicationMeta);
+            if (parameters == null)
+                return Maybe.Empty<CmdApplicationConfiguration>();
+
+            var configuration = new CmdApplicationConfiguration(
+                friendlyName,
+                applicationMeta.ApplicationName,
+                new System.Collections.ObjectModel.ReadOnlyCollection<IParameter>(parameters)
+                );
+
+            return null;
+        }
+
+        protected abstract Name GetFriendlyName(T data, CmdApplicationMeta applicationMeta);
+        protected abstract IList<IParameter> GetParameters(T data, CmdApplicationMeta applicationMeta);
+    }
+}
