b0df2ce524ec42ce9bd3ba375d7e0ca493394f7a
Rahul P Nath <rahulpnath@gmail.com>
2016-02-06 17:01:54 +1100

    Added ssms application meta in the test project

---
+++ src/CLAL.SSMS/CLALSsmsParser.UnitTest/SsmsCmdApplication.cs
@@ -0,0 +1,27 @@
+﻿using CommandLineApplicationLauncherModel;
+using System.Collections.Generic;
+
+namespace CLALSsmsParser.UnitTest
+{
+    public static class SsmsCmdApplication
+    {
+        public static CmdApplicationMeta Application
+        {
+            get
+            {
+                return new CmdApplicationMeta(
+                    (Name)"Sql Server",
+                    (Name)"ssms",
+                    new List<ParameterMeta>()
+                    {
+                        ParameterMeta.Create<NameValueParameter>((Name)"-S", (Name)"servername"),
+                        ParameterMeta.Create<NameValueParameter>((Name)"-d", (Name)"databasename"),
+                        ParameterMeta.Create<NameValueParameter>((Name)"-U", (Name)"username"),
+                        ParameterMeta.Create<NameValueParameter>((Name)"-P", (Name)"password"),
+                        ParameterMeta.Create<NameOnlyParameter>((Name)"-E", (Name)"windows auth"),
+                        ParameterMeta.Create<NameOnlyParameter>((Name)"-nosplash"),
+                    });
+            }
+        }
+    }
+}
