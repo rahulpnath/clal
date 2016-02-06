84ca33b8a883ebe292101894e3b7c64724e8c9b0
Rahul P Nath <rahulpnath@gmail.com>
2016-02-06 16:53:49 +1100

    Added parser project for SSMS
    Currently keeping this in the same repository. Might need to find a different model of delivering these packages, so that we can choose which parsers to take in. Maybe a package manager?

---
+++ src/CLAL.SSMS/CLALSsmsParser.UnitTest/ConnectionStringParserTests.cs
@@ -0,0 +1,20 @@
+﻿using CommandLineApplicationLauncherModel;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CLALSsmsParser.UnitTest
+{
+    public class ConnectionStringParserTests
+    {
+        [Theory, AutoData]
+        public void SutIsCmdApplicationConfigurationParser(ConnectionStringParser sut)
+        {
+            Assert.IsAssignableFrom<CmdApplicationConfigurationParser<string>>(sut);
+        }
+    }
+}
---
+++ src/CLAL.SSMS/CLALSsmsParser/ConnectionStringParser.cs
@@ -0,0 +1,19 @@
+﻿using CommandLineApplicationLauncherModel;
+using System;
+using System.Collections.Generic;
+
+namespace CLALSsmsParser
+{
+    public class ConnectionStringParser : CmdApplicationConfigurationParser<string>
+    {
+        protected override Name GetFriendlyName(string data, CmdApplicationMeta applicationMeta)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IList<IParameter> GetParameters(string data, CmdApplicationMeta applicationMeta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
