6a8eca3d363060ee9c354dc934bd8027db2e1a8d
Rahul P Nath <rahulpnath@gmail.com>
2016-02-10 05:25:28 +1100

    Added parse customization:

--- src/CommandLineApplicationLauncherUI.UnitTest/Helpers/AutoMoqDataAttribute.cs
+++ src/CommandLineApplicationLauncherUI.UnitTest/Helpers/AutoMoqDataAttribute.cs
@@ -1,13 +1,48 @@
-﻿using Ploeh.AutoFixture;
+﻿using System;
+using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoMoq;
 using Ploeh.AutoFixture.Xunit2;
+using CommandLineApplicationLauncherModel;
+using System.Collections.Generic;
 
 namespace CommandLineApplicationLauncherUI.UnitTest
 {
     public class AutoMoqDataAttribute : AutoDataAttribute
     {
-        public AutoMoqDataAttribute(): base(new Fixture().Customize(new AutoMoqCustomization()))
+        public AutoMoqDataAttribute()
+            : base(new Fixture().Customize(new AutoMoqCustomization()).Customize(new ParseCustomization()))
         {
         }
     }
+
+    public class ParseCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Inject<CmdApplicationConfigurationParser<string>>(new TestParser((Name)"Test", new List<IParameter>()));
+        }
+    }
+
+    class TestParser : CmdApplicationConfigurationParser<string>
+    {
+        public Name FriendlyName { get; private set; }
+        public List<IParameter> Parameters { get; private set; }
+
+        public TestParser(Name friendlyName, List<IParameter> parameters)
+        {
+            this.FriendlyName = friendlyName;
+            this.Parameters = parameters;
+        }
+
+        protected override Name GetFriendlyName(string data, CmdApplicationMeta applicationMeta)
+        {
+            return FriendlyName;
+        }
+
+        protected override IList<IParameter> GetParameters(string data, CmdApplicationMeta applicationMeta)
+        {
+            return Parameters;
+        }
+    }
+
 }
