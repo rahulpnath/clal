eff066b059d6e5551fcb7cf337aa6032cffdea6d
Rahul P Nath <rahulpnath@gmail.com>
2016-02-25 05:02:41 +1100

    Cleanup and remove setting new instance to configciewmodel

--- src/CommandLineApplicationLauncherViewModel/MainViewModel.cs
+++ src/CommandLineApplicationLauncherViewModel/MainViewModel.cs
@@ -1,23 +1,9 @@
-using System;
-using CommandLineApplicationLauncherModel;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 
 namespace CommandLineApplicationLauncherViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         public CmdApplicationConfigurationViewModel CmdApplicationConfigurationViewModel { get; private set; }
@@ -25,13 +11,11 @@ namespace CommandLineApplicationLauncherViewModel
 
         public CmdApplicationConfigurationListViewModel CmdApplicationConfigurationListViewModel { get; private set; }
 
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel(ICmdApplicationConfigurationViewModelFactory factory, CmdApplicationConfigurationListViewModel cmdApplicationConfigurationListViewModel)
+        public MainViewModel(
+            ICmdApplicationConfigurationViewModelFactory factory, 
+            CmdApplicationConfigurationListViewModel cmdApplicationConfigurationListViewModel)
         {
             this.CmdApplicationConfigurationListViewModel = cmdApplicationConfigurationListViewModel;
-            this.CmdApplicationConfigurationViewModel = factory.Create(SsmsCmdApplication.Application);
             this.AddCommand = new RelayCommand(this.OnAddExecuted);
         }
 
