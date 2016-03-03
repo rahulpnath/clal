82b568baf0cde64ce3b4b96547f01cf1c6a80df5
Rahul P Nath <rahulpnath@gmail.com>
2016-03-04 05:12:58 +1100

    Update to perform both replace on same file contents

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -6,5 +6,4 @@ $z29 = "./ExternalTools/ZeroToNine/Zero29.exe"
 
 # Update Appveyor.yml
 
-((Get-Content ./Appveyor.yml | Out-String) -replace "version: .*\.0", ("version: " + $version + ".0")).Trim("`r`n") | Set-Content -NoNewline Appveyor.yml
-((Get-Content ./Appveyor.yml | Out-String) -replace "version: .*\.{build}", ("version: " + $version + ".{build}")).Trim("`r`n") |  Set-Content -NoNewline Appveyor.yml
+((Get-Content ./Appveyor.yml | Out-String) -replace "version: .*\.0", ("version: " + $version + ".0") -replace "version: .*\.{build}", ("version: " + $version + ".{build}")).Trim("`r`n") |  Set-Content -NoNewline Appveyor.yml
