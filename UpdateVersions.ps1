c121671a11279432136b0710d71c1ceee43164f3
Rahul P Nath <rahulpnath@gmail.com>
2016-03-03 16:07:32 +1100

    Added powershell script

---
+++ UpdateVersions.ps1
@@ -0,0 +1,10 @@
+param([string]$version)
+
+# Update All AssemblyInfo file versions
+$z29 = "./ExternalTools/ZeroToNine/Zero29.exe"
+&$z29 -a $version
+
+# Update Appveyor.yml
+
+((Get-Content ./Appveyor.yml | Out-String) -replace "version: .*\.0", ("version: " + $version + ".0")).Trim("`r`n") | Set-Content -NoNewline Appveyor.yml
+((Get-Content ./Appveyor.yml | Out-String) -replace "version: .*\.{build}", ("version: " + $version + ".{build}")).Trim("`r`n") |  Set-Content -NoNewline Appveyor.yml
