2df2f795f865be55a2882efb1b5fcefd30f8ec28
Rahul P Nath <rahulpnath@gmail.com>
2016-03-23 04:40:20 +1100

    Handle mouse double click to launch

--- src/CommandLineApplicationLauncherUI/View/MainWindow.xaml.cs
+++ src/CommandLineApplicationLauncherUI/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using CommandLineApplicationLauncherViewModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CommandLineApplicationLauncherUI
 {
@@ -10,6 +12,18 @@ namespace CommandLineApplicationLauncherUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ApplicationConfigurationList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var senderAsListBox = sender as ListBox;
+            if (senderAsListBox != null)
+            {
+                var context = senderAsListBox.SelectedItem as CmdApplicationConfigurationViewModel;
+
+                if (context != null)
+                    context.Launch.Execute(null);
+            }
         }
     }
 }
